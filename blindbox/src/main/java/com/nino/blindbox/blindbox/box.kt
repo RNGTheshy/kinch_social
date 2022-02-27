bc4a7605dcb9ad2321e42c6e63a368fb8f007e83
Nino <2863089364@qq.com>
2022-02-27 15:58:51 +0800

    新增盲盒属性

---
+++ blindbox/src/main/java/com/nino/blindbox/blindbox/box.kt
@@ -0,0 +1,9 @@
+package com.nino.blindbox.blindbox
+
+import android.widget.ImageView
+
+class box(
+    val name: String = "",
+    var owned: Boolean = false,
+    val photo: ImageView? = null,
+)
