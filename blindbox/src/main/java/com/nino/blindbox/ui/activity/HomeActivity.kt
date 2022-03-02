f49fed9a355870242bfe36d152d2e47c9a1dd202
Nino <2863089364@qq.com>
2022-03-03 01:04:48 +0800

    设计展柜界面，取消底部导航栏的滑动动画以及滑动切换

--- blindbox/src/main/java/com/nino/blindbox/ui/activity/HomeActivity.kt
+++ blindbox/src/main/java/com/nino/blindbox/ui/activity/HomeActivity.kt
@@ -1,8 +1,10 @@
 package com.nino.blindbox.ui.activity
 
 import android.os.Bundle
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
@@ -12,7 +14,8 @@ import com.nino.blindbox.ui.fragment.*
 
 class HomeActivity : AppCompatActivity() {
     //标题
-    private val titles = arrayOf("首页", "市场", "玩具柜", "消息","个人中心")
+    private val titles = arrayOf("首页", "市场", "展柜", "消息","我的")
+
     private val fragmentList : MutableList<Fragment> = ArrayList()
 
 
@@ -44,6 +47,21 @@ class HomeActivity : AppCompatActivity() {
                 return  fragmentList[position]
             }
         }
+        //取消切换动画效果
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.position?.let { viewPager?.setCurrentItem(it, false) }
+            }
+
+        })
+        //取消滑动切换
+        viewPager.isUserInputEnabled=false
         viewPager.offscreenPageLimit = 3
         val tabLayoutMediator = TabLayoutMediator(
             tabLayout,viewPager, TabLayoutMediator.TabConfigurationStrategy { tab: TabLayout.Tab, position: Int ->
