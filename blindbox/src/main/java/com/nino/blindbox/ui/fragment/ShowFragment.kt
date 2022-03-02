3c116e78b12daae0d1099a5eb46a210116afaaed
Nino <2863089364@qq.com>
2022-03-03 00:36:38 +0800

    设计展柜界面，取消底部导航栏的滑动动画以及滑动切换

--- blindbox/src/main/java/com/nino/blindbox/ui/fragment/ShowFragment.kt
+++ blindbox/src/main/java/com/nino/blindbox/ui/fragment/ShowFragment.kt
@@ -5,10 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.nino.blindbox.R
+import com.nino.blindbox.ui.fragment.showFragment.TopToyFragment
+import com.nino.blindbox.ui.fragment.showFragment.Toy52Fragment
+import com.nino.blindbox.ui.fragment.showFragment.othersFragment
+import com.nino.blindbox.ui.fragment.showFragment.ppmtFragment
 
+class ShowFragment : Fragment() {
+    private val titles = arrayOf("POP MART", "52TOYS", "TOP TOY","其他")
+    private val fragmentList: MutableList<Fragment> = ArrayList()
 
-class ShowFragment : Fragment(){
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,6 +26,36 @@ class ShowFragment : Fragment(){
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         val view = LayoutInflater.from(context).inflate(R.layout.fragment_show, null)
+        initView(view)
         return view
     }
+
+    private fun initView(view :View) {
+        val viewPager = view?.findViewById(R.id.viewPager) as ViewPager2
+        val tabLayout = view?.findViewById(R.id.tab_layout) as TabLayout
+        //初始化fragment
+        fragmentList.add(ppmtFragment())
+        fragmentList.add(Toy52Fragment())
+        fragmentList.add(TopToyFragment())
+        fragmentList.add(othersFragment())
+        //初始化viewPage
+        viewPager!!.adapter = object : FragmentStateAdapter(this) {
+            override fun getItemCount(): Int {
+                return fragmentList.size
+            }
+
+            override fun createFragment(position: Int): Fragment {
+                return fragmentList[position]
+            }
+        }
+        viewPager.offscreenPageLimit = 3
+        val tabLayoutMediator = TabLayoutMediator(
+            tabLayout,
+            viewPager,
+            TabLayoutMediator.TabConfigurationStrategy { tab: TabLayout.Tab, position: Int ->
+                tab.text = titles[position]
+            })
+        tabLayoutMediator.attach()
+    }
 }
+
---
+++ blindbox/src/main/java/com/nino/blindbox/ui/fragment/showFragment/othersFragment.kt
@@ -0,0 +1,20 @@
+package com.nino.blindbox.ui.fragment.showFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nino.blindbox.R
+
+class othersFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val view = LayoutInflater.from(context).inflate(R.layout.show_fragment_detail, null)
+        return view
+    }
+}
