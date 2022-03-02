c0530cd8da6db9e91adee3149c75b7212ed5387b
Nino <2863089364@qq.com>
2022-03-02 23:44:59 +0800

    设计展柜界面，取消底部导航栏的滑动动画以及滑动切换

---
+++ blindbox/src/main/java/com/nino/blindbox/ui/fragment/showFragment/TopToyFragment.kt
@@ -0,0 +1,21 @@
+package com.nino.blindbox.ui.fragment.showFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nino.blindbox.R
+
+
+class TopToyFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val view = LayoutInflater.from(context).inflate(R.layout.show_fragment_detail, null)
+        return view
+    }
+}
