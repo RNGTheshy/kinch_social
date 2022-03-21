301b0bb23789a1c208ed5aaa8e965f321d5c773a
v_yubinma <v_yubinma@tencent.com>
2022-03-21 23:02:13 +0800

    登录页面重构

--- login/src/main/java/com/chaoshan/login/LoginActivity.kt
+++ login/src/main/java/com/chaoshan/login/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.chaoshan.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
@@ -12,5 +13,13 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = LoginMainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        binding.forgetPassword.setOnClickListener {
+            val intent = Intent(this, ForgetPasswordActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- login/src/main/java/com/chaoshan/login/LoginComeActivity.kt
+++ login/src/main/java/com/chaoshan/login/LoginComeActivity.kt
@@ -1,5 +1,6 @@
 package com.chaoshan.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
@@ -11,5 +12,17 @@ class LoginComeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = LoginComeActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initView();
+    }
+
+    private fun initView() {
+        binding.login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
