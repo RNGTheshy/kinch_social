912471e5428c28c5439e19e96d0a5deefe874a45
LCH <862432718@qq.com>
2022-03-29 02:48:52 +0800

    android聊天界面和信息界面实现

--- broadcast/src/main/java/com/example/broadcast/Msg.kt
+++
@@ -1,8 +0,0 @@
-package com.example.broadcast
-
-class Msg(val content: String, val type: Int) {
-    companion object {
-        const val TYPE_RECEIVED = 0
-        const val TYPE_SENT = 1
-    }
-}
---
+++ chat/src/main/java/com/example/chat/ChatActivity.kt
@@ -0,0 +1,49 @@
+package com.example.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class ChatActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val msgList = ArrayList<Msg>()
+    private var adapter: MsgAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initMsg()
+        val layoutManager = LinearLayoutManager(this)
+        recycler_view.layoutManager = layoutManager
+        adapter = MsgAdapter(msgList)
+        recycler_view.adapter = adapter
+        send.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            send -> {
+                val content = inputText.text.toString()
+                if (content.isNotEmpty()) {
+                    val msg = Msg(content, Msg.TYPE_SENT)
+                    msgList.add(msg)
+                    adapter?.notifyItemInserted(msgList.size - 1) // 当有新消息时，刷新RecyclerView中的显示
+                    recycler_view.scrollToPosition(msgList.size - 1)  // 将RecyclerView定位到最后一行
+                    inputText.setText("") // 清空输入框中的内容
+                }
+            }
+        }
+    }
+
+    private fun initMsg() {
+        val msg1 = Msg("Hello guy.", Msg.TYPE_RECEIVED)
+        msgList.add(msg1)
+        val msg2 = Msg("Hello. Who is that?", Msg.TYPE_SENT)
+        msgList.add(msg2)
+        val msg3 = Msg("This is Tom. Nice talking to you. ", Msg.TYPE_RECEIVED)
+        msgList.add(msg3)
+    }
+
+}
