5b9b28a83b4ab1eb75065b5f900f33a23546e3bb
v_yubinma <v_yubinma@tencent.com>
2022-04-20 09:28:26 +0800

    导入远程客户端

--- app_debug/src/main/java/com/yubinma/app_debug/DebugApplication.kt
+++ app_debug/src/main/java/com/yubinma/app_debug/DebugApplication.kt
@@ -1,10 +1,20 @@
 package com.yubinma.app_debug
 
 import androidx.multidex.MultiDexApplication
+import cn.leancloud.LCLogger
+import cn.leancloud.LeanCloud
+import cn.leancloud.LCObject
 
-class DebugApplication: MultiDexApplication() {
+
+class DebugApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
-
+        LeanCloud.initialize(
+            this,
+            "WFB1URKdIJqueBEjLd0P0xoy-gzGzoHsz",
+            "9uuBkty0jB2T7HXyqDWmLOVj",
+            "https://wfb1urkd.lc-cn-n1-shared.com"
+        )
+        LeanCloud.setLogLevel(LCLogger.Level.DEBUG);
     }
 }
