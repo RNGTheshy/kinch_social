5f75257b5f45c246aa209403ce449f4a5c47f2e5
yubinma <yubinma@noreply21929541.tapd.cn>
2022-05-12 12:15:05 +0000

    添加测试用户ID

--- app_debug/src/main/java/com/yubinma/app_debug/DebugApplication.kt
+++ app_debug/src/main/java/com/yubinma/app_debug/DebugApplication.kt
@@ -1,9 +1,15 @@
 package com.yubinma.app_debug
 
+import android.util.Log
 import androidx.multidex.MultiDexApplication
 import cn.leancloud.LCInstallation
 import cn.leancloud.LCLogger
 import cn.leancloud.LeanCloud
+import com.chaoshan.login.GetApplicationContext
+import com.chaoshan.login.SettingsPreferencesDataStore
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import cn.leancloud.chatkit.LCChatKit
 import cn.leancloud.im.LCIMOptions
 import cn.leancloud.push.PushService
@@ -12,6 +18,11 @@ import com.example.chat.CustomUserProvider
 
 
 class DebugApplication : MultiDexApplication() {
+    companion object {
+        const val USER_NAME = "user_name"
+    }
+
+    @DelicateCoroutinesApi
     override fun onCreate() {
         super.onCreate()
         LeanCloud.initialize(
@@ -21,6 +32,22 @@ class DebugApplication : MultiDexApplication() {
             "https://wfb1urkd.lc-cn-n1-shared.com"
         )
         LeanCloud.setLogLevel(LCLogger.Level.DEBUG);
+        GetApplicationContext.context = this.applicationContext
+        // 暂时测试时保留用户的ID
+        GlobalScope.launch {
+            SettingsPreferencesDataStore.saveData(
+                this@DebugApplication.applicationContext,
+                "6279d9b47a6d3118ac0283c5",
+                USER_NAME
+            )
+        }
+        // 获取数据
+        GetApplicationContext.context?.let {
+            GlobalScope.launch {
+                Log.e("nameTest", SettingsPreferencesDataStore.getName(it, "UserName"))
+            }
+        }
+
         LCChatKit.getInstance().setProfileProvider(CustomUserProvider.getInstance())
         LCIMOptions.getGlobalOptions().setDisableAutoLogin4Push(true)
         LCChatKit.getInstance().init(this,
@@ -36,6 +63,7 @@ class DebugApplication : MultiDexApplication() {
             println("---  $installationId")
         }
     }
+
 }
 
 
