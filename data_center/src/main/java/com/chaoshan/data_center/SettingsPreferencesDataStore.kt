05e06e9f73eb2ce4c69ed956167b1ffbf1069c6f
v_yubinma <v_yubinma@tencent.com>
2022-05-14 19:07:27 +0800

    完成登录

--- data_center/src/main/java/com/chaoshan/data_center/SettingsPreferencesDataStore.kt
+++ data_center/src/main/java/com/chaoshan/data_center/SettingsPreferencesDataStore.kt
@@ -1,12 +1,15 @@
 package com.chaoshan.data_center
 
 import android.content.Context
+import android.util.Log
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
 val Context.dataStore by preferencesDataStore(name = "settings")
 
@@ -33,11 +36,19 @@ object SettingsPreferencesDataStore {
     suspend fun getName(context: Context, key: String): String {
         EXAMPLE_COUNTER = stringPreferencesKey(key)
         val exampleCounterFlow: Flow<String> = context.dataStore.data
-                .map { preferences ->
-                    // No type safety.
-                    preferences[EXAMPLE_COUNTER] ?: "NULL"
-                }
+            .map { preferences ->
+                // No type safety.
+                preferences[EXAMPLE_COUNTER] ?: "NULL"
+            }
         return exampleCounterFlow.first()
+    }
+
+    suspend fun getUserObjectId(): String {
+        // 获取数据
+        GetApplicationContext.context?.let {
+            return getName(it, USER_NAME)
+        }
+        return "null"
     }
 }
 
