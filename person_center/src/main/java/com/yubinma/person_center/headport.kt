8b283406f23e8dc8e2b0af5bb6869f38cac2efe3
yubinma <yubinma@noreply21929541.tapd.cn>
2022-05-15 14:24:49 +0000

    Revert "Merge branch 'log10x' into 'master'"
    This reverts merge request !38

--- person_center/src/main/java/com/yubinma/person_center/headport.kt
+++ person_center/src/main/java/com/yubinma/person_center/headport.kt
@@ -48,7 +48,7 @@ public class Headport {
 
 
     //保存图片
-    private fun savepicture(objectid: String, bitmap: ByteArray) {
+    fun savepicture(objectid: String, bitmap: ByteArray) {
         val file = LCFile("test", bitmap)
         file.saveInBackground().subscribe(object : Observer<LCFile> {
             override fun onSubscribe(disposable: Disposable) {}
